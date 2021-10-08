947a259b35077cbecf426f71bef59cebf2f51cf7
Rutger Smit <rutger.smit@gmail.com>
2021-10-08 16:05:35 +0200

    initial commit

---
+++ src/content.js
@@ -0,0 +1,29 @@
+function find(){
+  var elems = document.querySelectorAll(
+    'div[data-react-class="ChallengeJoin"], div[data-react-class="FancyPromo"], div[data-testid="celebration_container"]'
+  );
+  elems.forEach((element) => {
+    removeParent(element)
+  });
+
+  elems = document.querySelectorAll(
+    'li[class="nav-item upgrade"]'
+  );
+  elems.forEach((element) => {
+    remove(element);
+  });
+  
+
+}
+
+function remove(elem) {
+  console.log("Remove parent element");
+  elem.remove();
+}
+
+function removeParent(elem) {
+  console.log("Remove element");
+  elem.parentElement.remove();
+}
+
+find();
