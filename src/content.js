e54883521083121f4346ccc7c5e9e93661091ab4
Rutger Smit <rutger.smit@gmail.com>
2021-11-11 09:30:46 +0100

    Added removal of the footer

--- src/content.js
+++ src/content.js
@@ -7,7 +7,7 @@ function find(){
   });
 
   elems = document.querySelectorAll(
-    'li[class="nav-item upgrade"], div[class="modal-backdrop fade in"], div[id="flybyModal"], div[id="your-challenges"]'
+    'li[class="nav-item upgrade"], div[class="modal-backdrop fade in"], div[id="flybyModal"], div[id="your-challenges"], div[class="section sidebar-footer"]'
   );
   elems.forEach((element) => {
     remove(element);
