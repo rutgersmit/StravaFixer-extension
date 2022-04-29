86806295f111f07e99e2520eb7a0d737b717369f
Rutger Smit <rutger.smit@gmail.com>
2022-04-29 22:53:17 +0200

    notification work in progress

--- src/content.js
+++ src/content.js
@@ -80,3 +80,34 @@ function parseActivities(data) {
   var doc = parser.parseFromString(data, "text/html");
   div.append(doc.querySelector("ul.recent-activities"));
 }
+
+
+
+//notificationsDiv.id = "notifications-copy";
+
+// notificationsDiv = document.querySelector('li[id="notifications-copy"]');
+// notificationsDiv.querySelector('button[id="notifications-button"]').remove();
+// //notificationsDiv.querySelector('div[class="notifications-drop-down"]').display =  "block";
+var notificationsLi = document.querySelector('li[id="notifications"]');
+var targetDiv = document.querySelector('div[class="col-md-3"]');
+
+var nUl = document.createElement('ul');
+nUl.appendChild(notificationsLi);
+
+var nDiv = document.createElement("div");
+nDiv.id = "your-notifications";
+nDiv.classList.add("section");
+
+var h3 = document.createElement("h3");
+h3.appendChild(document.createTextNode("Your notifications"));
+h3.className = 'text-callout section-title';
+nDiv.appendChild(h3);
+nDiv.appendChild(nUl);
+
+targetDiv.insertBefore(nDiv, targetDiv.children[0]);
+targetDiv.querySelector('button[id="notifications-button"]').remove();
+targetDiv.querySelector('div[id="notifications-drop-down"]').style.display =  "block";
+targetDiv.querySelector('div[id="notifications-drop-down"]').style.position='';
+targetDiv.querySelector('div[id="notifications-drop-down"]').style.border = "";
+targetDiv.querySelector('div[id="notifications-drop-down"]').style.width = "";
+targetDiv.querySelector('div[id="notifications-drop-down"]').style.top = "";
